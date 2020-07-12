1306b8639a934d58f8412cd061aec158c5f6396a
ashdeo <ashishdeos@gmail.com>
2020-07-12 18:19:05 +0530

    Add Parameter feelslike and humidity on home page weather response JSON

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,13 @@
 const request = require('postman-request')
 
+
+//Goal : Add new data to forecast 
+// 
+//1. Update the forecast string to include new data
+//2. Commit your changes
+//3. Push your changes to Github and deploy to Heroku 
+//4. Test your work in the live application!
+
 const forecast = ( (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=67e2b983509d17fd87d07b816e1ca026&query='+latitude + ', '+longitude
     request({ url: url, json: true },(error, response) => {
@@ -16,7 +24,7 @@ const forecast = ( (latitude, longitude, callback) => {
  
         callback(undefined,response.body.current.weather_descriptions[0] +'. It is currently '+ 
         response.body.current.temperature + ' degress out. Ther is a ' + 
-        response.body.current.precip +' % change of rain')
+        response.body.current.precip +' % change of rain'+'\n'+ 'It is currently '+response.body.current.temperature +' It Feels like its '+response.body.current.feelslike+'\n'+'Humudity is '+response.body.current.humidity+'Percentage' )
         
         // callback(undefined,{
         //     temperature: response.body.current.temperature,
